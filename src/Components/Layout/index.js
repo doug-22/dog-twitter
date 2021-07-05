bec84af60fa095159e996df383a669f4157c2c39
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-07-05 15:39:25 -0300

    Adicionando componente layout

---
+++ src/Components/Layout/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Main from "../Main"
+import MenuBar from "../MenuBar"
+
+import { Container, Wrapper } from "./styles"
+
+function Layout () {
+
+  return (
+    <Container>
+      <Wrapper>
+        <MenuBar />
+        <Main />
+        {/* <SideBar /> */}
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Layout;
