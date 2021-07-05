c0f9477accd1aa7635ceb957e881c0e94d8a44b9
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-07-05 15:38:07 -0300

    Adicionando global styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import GlobalStyles from "./Styles/GlobalStyles"
+
+import Layout from "./Components/Layout"
+
+
+class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     dogs: []
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   fetch("https://dog.ceo/api/breeds/image/random/10")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       this.setState({ dogs: data.message})
+  //     })
+  // }
+
+
+  render() {
+
+
+    return (
+      <>
+        <Layout />
+        <GlobalStyles />
+      </>
+    )
+  }
 }
 
 export default App;
