2d207c6f107125d3c74fbc0020b0f83499a92043
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-07-05 15:44:33 -0300

    Adicionando informações de perfil

---
+++ src/Components/Infos/perfilInfo.js
@@ -0,0 +1,51 @@
+export const Nomes = [
+    <>Apolo</>,
+    <>Dino</>,
+    <>Fenix</>,
+    <>Toddy</>,
+    <>José</>,
+    <>Sol</>,
+    <>Caramelo</>,
+    <>Juju</>,
+    <>Fred</>,
+    <>Billy</>
+]
+
+export const Nickname = [
+    <>@apolo</>,
+    <>@dino</>,
+    <>@fenix</>,
+    <>@tod</>,
+    <>@zé</>,
+    <>@sol</>,
+    <>@caramelo</>,
+    <>@ju</>,
+    <>@fred</>,
+    <>@billy</>
+]
+
+export const Descricao = [
+    <>Visitando meus tios na fazenda! au au au</>,
+    <>Dia de passeio</>,
+    <>Dia de passeio</>,
+    <>Eu e meus humanos</>,
+    <>Fazendo pose natural</>,
+    <>Tomando um banho de sol</>,
+    <>Passeio do dia</>,
+    <>Exercício pago</>,
+    <>Esse dia foi louco</>,
+    <>Treino tá pago</>
+]
+
+export const Data = [
+    <>22 de jun</>,
+    <>28 de dez</>,
+    <>30 de jan</>,
+    <>13 de abr</>,
+    <>31 de out</>,
+    <>20 de nov</>,
+    <>04 de mar</>,
+    <>05 de ago</>,
+    <>29 de fev</>,
+    <>07 de set</>
+]
